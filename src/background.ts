f4dd71f0328125b858204e3915e172a1c0634523
v1xingyue <qixingyue@gmail.com>
2024-02-02 23:50:11 +0800

    使用 async

--- src/background.ts
+++ src/background.ts
@@ -5,13 +5,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (type === "load") {
     console.log("load from -> ", content);
     const { url } = content;
-    fetch(url).then((response) => {
-      response.text().then((text) => {
-        const payload = { status: response.status, text };
-        console.log("load response -> ", payload);
-        sendResponse(payload);
-      });
-    });
+    const loadUrl = async (url: string) => {
+      const response = await fetch(url);
+      const text = await response.text();
+      const payload = { status: response.status, text };
+      console.log("load response -> ", payload);
+    };
+    loadUrl(url);
   }
   return true;
 });
